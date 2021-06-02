1febf33242dc4db4192c8578358794f811d7e7ae
Stephen Gardener <stephen@stega.info>
2021-06-02 16:36:33 +0200

    use local time in db

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Conf
     # in config/environments, which are processed later.
     #
     config.time_zone = "Stockholm"
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
