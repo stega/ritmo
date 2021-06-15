32275ea95eeb6c3d1703fedc200f1a63bb0f9b99
Stephen Gardener <stephen@stega.info>
2021-06-15 11:43:41 +0200

    removed cors stuff

--- Gemfile
+++ Gemfile
@@ -35,7 +35,6 @@ gem 'pg'
 gem 'bootstrap_icons_rails'
 gem 'pg_search'
 gem 'kaminari'
-gem 'rack-cors'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -19,12 +19,5 @@ module Conf
     config.time_zone = "Stockholm"
     config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
-      end
-    end
   end
 end
